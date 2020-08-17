276dc372b5a011cac59876e73be959b4869d9041
vojtechmolek@gmail.com <vojtechmolek@gmail.com>
2020-08-17 15:05:37 +0200

    Rewriting according to Peters implementation 10 epochs acc 44.62% which is the worst out of the 4 tested network.

--- custom_op/user_ops/kernels/ft_pool_kernel.cc
+++ custom_op/user_ops/kernels/ft_pool_kernel.cc
@@ -13,8 +13,6 @@ namespace functor {
 template <typename Dtype>
 struct FtPoolFunctor<CPUDevice, Dtype> {
     static void launch(::tensorflow::OpKernelContext* ctx, const Tensor& input, Tensor* output, std::vector<float> stride, std::vector<float> pool_size) {
-        //std::cout.precision(2);
-        //std::cout << "dtype is: " << typeid(Dtype).name() << std::endl;
         const auto in_tensor = input.tensor<Dtype, 4>();
         auto out_tensor = output->tensor<Dtype, 4>();
         out_tensor.setZero();
@@ -24,74 +22,46 @@ struct FtPoolFunctor<CPUDevice, Dtype> {
         double bf_sum = 0, comp = 0, power_x  = 0, power_y  = 0, bf_value, hh, ww;
         double width_h  = static_cast<double>(pool_size[1]) / 2.0;
         double height_h = static_cast<double>(pool_size[0]) / 2.0;
-        double bf_arr_w = floor(2*width_h), bf_arr_h = floor(2*height_h);
+        double bf_arr_w = ceil(2*width_h), bf_arr_h = ceil(2*height_h);
         std::array<float, 100> bf_values;
 
-        //std::cout << "****************Forward pass****************" << std::endl;
-        //std::cout << "****************input tensor****************" << std::endl;
-        for (int y = 0; y < H; y += 1){
-            for (int x = 0; x < W; x += 1){
-                //std::cout << in_tensor(0,y,x,0) << "\t";
-            }
-            //std::cout << std::endl;
-        }
-        //std::cout << "****************input tensor****************" << std::endl;
         for (double h = 0; h < H; h += stride[0]){
-            hh = ceil(h-height_h);
+            hh = (int)h-height_h;
             for (double w = 0; w < W; w += stride[1]){
-                ww = ceil(w-width_h);
-                /************ compute basic fc sum ***********/
-                //std::cout << "****************basic fnc[" << h << "," << w << "]****************" << std::endl;
+                ww = (int)w-width_h;
                 if (round(w/stride[1]) < output->dim_size(2) && round(h/stride[0]) < output->dim_size(1)){
                     bf_sum = 0;
-                    for (double y=hh; y<=floor(h+height_h); y++){
+                    comp = 0;
+                    for (double y=hh; y<=(int)h+height_h; y++){
                         if (round(y) >= H) break;
                         if (round(y) < 0) continue;
-                        for (double x=ww; x<=floor(w+width_h); x++){
+                        for (double x=ww; x<=(int)w+width_h; x++){
                             if (round(x) >= W) break;
                             if (round(x) < 0) continue;
-                            power_x = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(x-w) / width_h)) / 2 );
-                            power_y = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(y-h) / height_h)) / 2 );
+                            power_x = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(x-(int)w) / width_h)) / 2 );
+                            power_y = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(y-(int)h) / height_h)) / 2 );
                             bf_value = power_x * power_y;
                             bf_sum  += bf_value;
                             bf_values[(y-hh)*bf_arr_w + (x-ww)] = bf_value;
-                            //std::cout << bf_values[(y-hh)*bf_arr_w + (x-ww)] << "\t";
                         }
-                        //std::cout << std::endl;
                     }
-                    //std::cout << "****************basic fnc****************" << std::endl;
-                    //std::cout << "****************components****************" << std::endl;
-                /************ compute component at x,y for all channels and data samples in batch ***********/
                     for (int n = 0; n < N; n++){
                         for (int c = 0; c < C; c++){
-                            if (!bf_sum){
-                                out_tensor(n, round(h/stride[0]), round(w/stride[1]), c) = 0;
-                            }else{
-                            comp = 0;
-                            for (double y=hh; y<=floor(h+height_h); y++){
+                            for (double y=hh; y<=(int)h+height_h; y++){
                                 if (round(y) >= H) break;
                                 if (round(y) < 0) continue;
-                                for (double x=ceil(w-width_h); x<=floor(w+width_h); x++){
+                                for (double x=ww; x<=(int)w+width_h; x++){
                                     if (round(x) >= W) break;
                                     if (round(x) < 0) continue;
-                                    //power_x = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(x-w) / width_h)) / 2 );
-                                    //power_y = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(y-h) / height_h)) / 2 );
-                                    //bf_value = power_x * power_y;
-                                    comp += in_tensor(n, static_cast<int>(y), static_cast<int>(x), c) * 
-                                    bf_values[(y-hh)*bf_arr_w + (x-ww)];
+                                    out_tensor(n, round(h/stride[0]), round(w/stride[1]), c) += bf_values[(y-hh)*bf_arr_w + (x-ww)] * in_tensor(n, round(y), round(x), c);
                                 }
                             }
-                                out_tensor(n, round(h/stride[0]), round(w/stride[1]), c) = comp/bf_sum;
-                                //std::cout << out_tensor(n, round(h/stride[0]), round(w/stride[1]), c) << "(" << comp/bf_sum << ")\t";
-                            }
+                            out_tensor(n, round(h/stride[0]), round(w/stride[1]), c) /= bf_sum;
                         }
-                        //std::cout << std::endl;
                     }
-                    //std::cout << "****************components****************" << std::endl;
                 }
             }
         }
-        //std::cout << "****************Forward pass****************" << std::endl;
     }
 };
 
@@ -116,94 +86,47 @@ struct FtPoolGrad<CPUDevice, Dtype> {
         double bf_sum = 0, power_x, power_y, bf_value, hh, ww;
         double width_h  = static_cast<double>(pool_size[1]) / 2.0;
         double height_h = static_cast<double>(pool_size[0]) / 2.0;
-        const int bf_arr_w = floor(2*width_h), bf_arr_h = floor(2*height_h);
+        const int bf_arr_w = ceil(2*width_h), bf_arr_h = ceil(2*height_h);
         std::array<float, 100> bf_values;
-        //std::cout << "****************Backward pass****************" << std::endl;
-        //std::cout << "****************grad in****************" << std::endl;
-        for (int y = 0; y < grad_in.dim_size(1); y += 1){
-            for (int x = 0; x < grad_in.dim_size(2); x += 1){
-                //std::cout << grad_in_tensor(0,y,x,0) << "\t";
-            }
-            //std::cout << std::endl;
-        }
-        //std::cout << "****************grad in****************" << std::endl;
-        //std::cout << "****************grad out****************" << std::endl;
-        for (int y = 0; y < grad_out->dim_size(1); y += 1){
-            for (int x = 0; x < grad_out->dim_size(2); x += 1){
-                //std::cout << grad_out_tensor(0,y,x,0) << "\t";
-            }
-            //std::cout << std::endl;
-        }
-        //std::cout << "****************grad out****************" << std::endl;
-        //std::cout << "****************grad in distribution****************" << std::endl;
+        
         for (double h = 0; h < H; h += stride[0]){
-            hh = ceil(h-height_h);
+            hh = (int)h-height_h;
             for (double w = 0; w < W; w += stride[1]){
-                ww = ceil(w-width_h);
-                /************ compute basic fc sum ***********/
+                ww = (int)w-width_h;
                 if (round(w/stride[1]) < grad_in.dim_size(2) && round(h/stride[0]) < grad_in.dim_size(1)){
                     bf_sum = 0;
-                    for (double y=hh; y<=floor(h+height_h); y++){
+                    for (double y=hh; y<=(int)h+height_h; y++){
                         if (round(y) >= H) break;
                         if (round(y) < 0) continue;
-                        for (double x=ww; x<=floor(w+width_h); x++){
+                        for (double x=ww; x<=(int)w+width_h; x++){
                             if (round(x) >= W) break;
                             if (round(x) < 0) continue;
-                            power_x = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(x-w) / width_h)) / 2 );
-                            power_y = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(y-h) / height_h)) / 2 );
+                            power_x = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(x-(int)w) / width_h)) / 2 );
+                            power_y = sin( 3.14*static_cast<double>(1.0 - static_cast<double>(std::abs(y-(int)h) / height_h)) / 2 );
                             bf_value = power_x * power_y;
+                            bf_sum  += bf_value;
                             bf_values[(y-hh)*bf_arr_w + (x-ww)] = bf_value;
-                            //bf_sum  += bf_value;
-                            sum_tensor(0,y,x,0) += bf_value;
                         }
                     }
-                    //if (!bf_sum) bf_sum = 1.0;
-                    /************ distribute gradient ***********/
-                    //std::cout << "****************grad in[" << h << "," << w << "] distribution****************" << std::endl;
                     for (int n = 0; n < N; n++){
-                        for(int c = 0; c < C; c++){
-                            for (double y=hh; y<=floor(h+height_h); y++){
+                        for (int c = 0; c < C; c++){
+                            for (double y=hh; y<=(int)h+height_h; y++){
                                 if (round(y) >= H) break;
                                 if (round(y) < 0) continue;
-                                for (double x=ww; x<=floor(w+width_h); x++){
+                                for (double x=ww; x<=(int)w+width_h; x++){
                                     if (round(x) >= W) break;
                                     if (round(x) < 0) continue;
-                                    //power_x = sin( 3.14*static_cast<double>(1 - static_cast<double>(std::abs(x-(w)) / width_h)) / 2 );
-                                    //power_y = sin( 3.14*static_cast<double>(1 - static_cast<double>(std::abs(y-(h)) / height_h)) / 2 );
-                                    //if (!power_x || !power_y) continue;
-                                    //bf_value = power_x * power_y;
-                                    //std::cout << grad_out_tensor(n, y, x, c) << "+";
-                                    grad_out_tensor(n, y, x, c) += grad_in_tensor(n, round(h/stride[0]), round(w/stride[1]), c) * bf_values[(y-hh)*bf_arr_w + (x-ww)];
-                                    //std::cout << grad_in_tensor(n, round(h/stride[0]), round(w/stride[1]), c) << "*" << bf_values[(y-hh)*bf_arr_w + (x-ww)] << "=";
-                                    //std::cout << grad_out_tensor(n, y, x, c) << "\t";
-                                        //bf_values[yy + (x-ww)]) / bf_sum;
+                                    grad_out_tensor(n, round(y), round(x), c) += bf_values[(y-hh)*bf_arr_w + (x-ww)] * grad_in_tensor(n, round(h/stride[0]), round(w/stride[1]), c);
                                 }
-                                //std::cout << std::endl;
                             }
                         }
                     }
-                    //std::cout << "****************grad in[" << h << "," << w << "] distribution****************" << std::endl;
                 }
-            }
-        }/*
-        //std::cout << "****************grad out****************" << std::endl;
-        for (int n=0; n<N; n++){
-            for (int h=0; h<H; h++){
-                for (int w=0; w<W; w++){
-                    for (int c=0; c<C; c++){
-                        //std::cout << grad_out_tensor(n, h, w, c) << "/" << sum_tensor(n, h, w, c) << "\t";
-                        if (!sum_tensor(0,h, w,0)) sum_tensor(0,h, w,0) = 1.0;
-                            grad_out_tensor(n, h, w, c) /= sum_tensor(0,h, w,0);
-                    }
-                }
-                //std::cout << std::endl;
             }
         }
-        //std::cout << "****************grad out****************" << std::endl;
-        */
     }
 };
-
+ 
 template struct FtPoolGrad<CPUDevice, float>;
 template struct FtPoolGrad<CPUDevice, double>;
 
